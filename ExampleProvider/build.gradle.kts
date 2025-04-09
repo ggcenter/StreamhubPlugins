de08132622dd6be61f32f23d94a2ec4c5de62274
marek <ggcenter@gmail.com>
2025-04-09 19:23:36 +0200

    update

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -38,8 +38,4 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
-    compileOptions {
-        sourceCompatibility JavaVersion.VERSION_17
-        targetCompatibility JavaVersion.VERSION_17
-    }
 }
